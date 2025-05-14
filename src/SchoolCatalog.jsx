fc63c6a848982e822e0af47c26311b77b27ba087
dacods <135403583+dacods@users.noreply.github.com>
2025-05-14 08:53:26 -0500

    Made the table sortable in ascending or descending order

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 export default function SchoolCatalog() {
   const [courses, setCourses] = useState([]);
   const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("trimester");
+  const [direction, setDirection] = useState("asc");
+
 
   useEffect(() => {
       fetch('/api/courses.json').then(res => {
@@ -18,6 +21,30 @@ export default function SchoolCatalog() {
     );
   });
 
+  const sortedCourses = courses.sort((a, b) => {
+    if (sort === "trimester") {
+      return a[sort].localeCompare(b.trimester) * (direction === "desc" ? -1: 1);
+    }
+    if (sort === "courseNumber") {
+      return a[sort].localeCompare(b.courseNumber) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "courseName") {
+      return a[sort].localeCompare(b.courseName) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "semesterCredits") {
+      return a[sort].localeCompare(b.semesterCredits) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "totalClockHours") {
+      return a[sort].localeCompare(b.totalClockHours) * (direction === "desc" ? -1 : 1);
+    }
+  });
+
+  const handleSortingChange = (field) => {
+    const sortOrder = sort === field && direction === "asc" ? "desc" : "asc";
+    setSort(field);
+    setDirection(sortOrder);
+  };
+
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
@@ -25,16 +52,16 @@ export default function SchoolCatalog() {
       <table>
         <thead>
           <tr>
-            <th>Trimester</th>
-            <th>Course Number</th>
-            <th>Courses Name</th>
-            <th>Semester Credits</th>
-            <th>Total Clock Hours</th>
+            <th onClick={() => handleSortingChange("trimester")}>Trimester</th>
+            <th onClick={() => handleSortingChange("courseNumber")}>Course Number</th>
+            <th onClick={() => handleSortingChange("courseName")}>Courses Name</th>
+            <th onClick={() => handleSortingChange("semesterCredits")}>Semester Credits</th>
+            <th onClick={() => handleSortingChange("totalClockHours")}>Total Clock Hours</th>
             <th>Enroll</th>
           </tr>
         </thead>
         <tbody>
-          {filteredCourses.map(course => (
+          {sortedCourses.map(course => (
             <tr key={course.courseNumber}>
               <td>{course.trimester}</td>
               <td>{course.courseNumber}</td>
