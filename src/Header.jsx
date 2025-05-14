6c63bbf3433da233ed9c0a042d436363f01b8f5e
dacods <135403583+dacods@users.noreply.github.com>
2025-05-14 16:15:17 -0500

    Header now tracks how many enrolled classes there are

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,16 @@
 import logo from "./assets/logo.png";
+import { useContext } from "react";
+import { EnrolledContext } from "./App";
 
 export default function Header() {
+  const { enrolled } = useContext(EnrolledContext);
+
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
-      <div className="enrollment">Classes Enrolled: 1</div>
+      <div className="enrollment">
+        Classes Enrolled: {enrolled.length}
+      </div>
     </div>
   );
 }
